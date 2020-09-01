b32a709a3bbb864887a8c457ffff486fdb84ef37
Trond Makonese <trondation@gmail.com>
2020-09-01 10:34:50 -0600

    Create login template.

---
+++ Login/Login.js
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+
+export default function Login() {
+
+  return (
+    <View>
+        <section>
+            <h1>made it</h1>
+        </section>
+    </View>
+  );
+}
